5e772f8a549ac35155d2fb4def17e519549b495f
Alpha Olomi <alphaolomi@gmail.com>
2023-03-10 14:43:53 +0300

    :white_check_mark: tests: add /hello test

--- routes/hello.dart
+++ routes/hello.dart
@@ -1,10 +1,13 @@
 import 'package:dart_frog/dart_frog.dart';
 
+// curl http://localhost:8080/hello
 Response onRequest(RequestContext context) {
   // Access the incoming request.
-  final request = context.request;
+  // final request = context.request;
 
   // Do stuff with the incoming request...
+  // Log the request.
+  // print('Request: ${request.method} ${request.uri}');
 
   // Return a response.
   return Response(body: 'Hello World');
--- test/routes/hello_test.dart
+++ test/routes/hello_test.dart
@@ -4,22 +4,22 @@ import 'package:dart_frog/dart_frog.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
-import '../../routes/index.dart' as route;
+import '../../routes/hello.dart' as route;
 
 class _MockRequestContext extends Mock implements RequestContext {}
 
 void main() {
-  group('GET /', () {
-    test('responds with a 200 and "Welcome to Dart Frog!".', () {
-        // Arrange
+  group('GET /hello', () {
+    test('responds with a 200 and "Hello World"', () {
+      // Arrange
       final context = _MockRequestContext();
-       // Act
+      // Act
       final response = route.onRequest(context);
       // Assert
       expect(response.statusCode, equals(HttpStatus.ok));
       expect(
-          response.body(),
-        completion(equals('Welcome to Dart Frog!')),
+        response.body(),
+        completion(equals('Hello World')),
       );
     });
   });
