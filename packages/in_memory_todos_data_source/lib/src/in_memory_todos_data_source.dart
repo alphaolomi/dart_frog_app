78840dce0cdc490eba54a8ba939790de94c4d0b8
Alpha Olomi <alphaolomi@gmail.com>
2023-03-10 15:15:29 +0300

    :tada: in_memory_todos_data_source initial commit

---
+++ packages/in_memory_todos_data_source/lib/src/in_memory_todos_data_source.dart
@@ -0,0 +1,7 @@
+/// {@template in_memory_todos_data_source}
+/// An in-memory implementation of the TodosDataSource interface.
+/// {@endtemplate}
+class InMemoryTodosDataSource {
+  /// {@macro in_memory_todos_data_source}
+  const InMemoryTodosDataSource();
+}
