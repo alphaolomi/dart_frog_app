3d341685e89e929f8b9be3c49d6188793b80a243
Alpha Olomi <alphaolomi@gmail.com>
2023-03-10 15:19:03 +0300

    :sparkles: implement InMemoryTodosDataSource

--- packages/in_memory_todos_data_source/lib/src/in_memory_todos_data_source.dart
+++ packages/in_memory_todos_data_source/lib/src/in_memory_todos_data_source.dart
@@ -1,7 +1,38 @@
-/// {@template in_memory_todos_data_source}
-/// An in-memory implementation of the TodosDataSource interface.
-/// {@endtemplate}
-class InMemoryTodosDataSource {
-  /// {@macro in_memory_todos_data_source}
-  const InMemoryTodosDataSource();
+// /// {@template in_memory_todos_data_source}
+// /// An in-memory implementation of the TodosDataSource interface.
+// /// {@endtemplate}
+// class InMemoryTodosDataSource {
+//   /// {@macro in_memory_todos_data_source}
+//   const InMemoryTodosDataSource();
+// }
+
+import 'package:todos_data_source/todos_data_source.dart';
+import 'package:uuid/uuid.dart';
+
+/// An in-memory implementation of the [TodosDataSource] interface.
+class InMemoryTodosDataSource implements TodosDataSource {
+  /// Map of ID -> Todo.
+  final _cache = <String, Todo>{};
+
+  @override
+  Future<Todo> create(Todo todo) async {
+    final id = const Uuid().v4();
+    final createdTodo = todo.copyWith(id: id);
+    _cache[id] = createdTodo;
+    return createdTodo;
+  }
+
+  @override
+  Future<List<Todo>> readAll() async => _cache.values.toList();
+
+  @override
+  Future<Todo?> read(String id) async => _cache[id];
+
+  @override
+  Future<Todo> update(String id, Todo todo) async {
+    return _cache.update(id, (value) => todo);
+  }
+
+  @override
+  Future<void> delete(String id) async => _cache.remove(id);
 }
